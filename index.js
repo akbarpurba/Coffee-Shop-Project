ae9089c57af527bea60a464b6a4678139b5b04c6
akbarpurba <akbarpurba499@gmail.com>
2025-09-09 02:47:49 +0700

    tambah animasi

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const hamburger = document.getElementById("hamburger");
 const nav = document.querySelector("section nav");
 
@@ -6,7 +7,6 @@ hamburger.addEventListener("click", () => {
     nav.classList.remove("active");
     nav.classList.add("closing");
 
-    // hapus class closing setelah animasi selesai
     nav.addEventListener("animationend", function onEnd() {
       nav.classList.remove("closing");
       nav.removeEventListener("animationend", onEnd);
@@ -21,3 +21,37 @@ hamburger.addEventListener("click", () => {
 
 document.getElementById('year').innerHTML = new Date().getFullYear();
  
+window.addEventListener("scroll", function() {
+  const header = document.querySelector("header");
+  const btn = document.querySelector(".btn button"); 
+  const nav = document.querySelector("nav");
+  const listItems = document.querySelectorAll("nav li"); 
+  
+  if (window.scrollY > 50) { 
+    header.classList.add("scrolled");
+    nav.classList.add("scrolled");
+    btn.classList.add("scroll");
+    listItems.forEach(li => li.classList.add("scrolled")); 
+  } else {
+    header.classList.remove("scrolled");
+    nav.classList.remove("scrolled");
+    btn.classList.remove("scroll");
+    listItems.forEach(li => li.classList.remove("scrolled")); 
+  }
+});
+
+new TypeIt("#typeIt", {
+  speed: 50,
+  waitUntilVisible: true,
+  loop: true,
+})
+.type("Selamat datang di AkbarCoffee!")
+.pause(1500)
+.delete()
+.type("Nikmati kopi terbaik setiap hari ☕")
+.pause(1500)
+.delete()
+.type("Suasana hangat, tempat nyaman, kopi istimewa.")
+.pause(2000)
+.delete()
+.go();
